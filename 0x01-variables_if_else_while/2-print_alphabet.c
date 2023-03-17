fb76a5c2a33de282148f7e10f2eb4b509df2d821
Happy Banda <hbfl3x@gmail.com>
2023-03-17 14:20:15 -0700

    Add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
