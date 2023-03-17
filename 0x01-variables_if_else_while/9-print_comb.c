6e5c8ad535b931c3fed9cfe5987b2468fa0d2eaf
Happy Banda <hbfl3x@gmail.com>
2023-03-17 15:40:13 -0700

    add 9-print_com.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of single-digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a + ', ');
+	}
+	putchar('\n');
+
+	return (0);
+}
