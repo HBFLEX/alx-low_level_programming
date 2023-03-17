7bd629b8c3608073848ebf5202d7c0850f5e560d
Happy Banda <hbfl3x@gmail.com>
2023-03-17 14:58:32 -0700

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers of base 10 from 0
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+		printf("%d\n", i);
+
+	return (0);
+}
