3b29b3cfb1d1c5f5321a63e802e2bcad1dceecdd
Happy Banda <hbfl3x@gmail.com>
2023-03-16 17:11:59 -0700

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - it prints a text using stdout puts function
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
