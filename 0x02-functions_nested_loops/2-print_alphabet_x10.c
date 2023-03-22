84c0a8ff3f2e0fc72af44ffc3d384f8fd6241a65
Happy Banda <hbfl3x@gmail.com>
2023-03-22 12:26:51 -0700

    add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10x in lowercase
+ * Return: 0 always
+*/
+void print_alphabet_x10(void)
+{
+	int a = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
+}
