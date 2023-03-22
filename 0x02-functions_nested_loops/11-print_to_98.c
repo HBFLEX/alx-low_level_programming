d46b263b6ce7dfcac72db222c9883b2952c58444
Happy Banda <hbfl3x@gmail.com>
2023-03-22 16:52:22 -0700

    hotfix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,8 +7,6 @@
 */
 void print_to_98(int n)
 {
-	int i;
-
 	if (n <= 98)
 	{
 		for (; n <= 98; n++)
@@ -33,7 +31,6 @@ void print_to_98(int n)
 			if (n == 98)
 			{
 				printf("%d\n", n);
-				break;
 			}
 
 			else
