067ae4f971bf212ae880589bb3a9deb358301d56
Happy Banda <hbfl3x@gmail.com>
2023-03-22 14:43:28 -0700

    add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute in 24 hours
+ * Return: 0 always
+*/
+void jack_bauer(void)
+{
+	int minute = 0;
+	int hour = 0;
+
+	while (hour < 24)
+	{
+		while (minute < 60)
+		{
+			_putchar(minute + '0');
+			_putchar(minute + '0');
+			_Putchar(':');
+			_putchar(hour + '0');
+			_putchar(hour + '0');
+			_putchar('\n');
+			minute++;
+		}
+		hour++;
+	}
+}
