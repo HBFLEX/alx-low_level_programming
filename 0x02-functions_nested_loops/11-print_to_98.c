3c8865b1c6d1b56227ddad15abaffb8851c08854
Happy Banda <hbfl3x@gmail.com>
2023-03-22 15:57:56 -0700

    add 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all numbers from n - 98
+ * @n: the number passed
+ * Return: 0 always
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		if (i != 98)
+		{
+			_putchar(i + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		else
+		{
+			_putchar(i);
+		}
+
+		i++;
+	}
+}
