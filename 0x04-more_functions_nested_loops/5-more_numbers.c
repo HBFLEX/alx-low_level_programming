b7e43acfdfd1dcf84535db473307da1bf09535c8
Happy Banda <hbfl3x@gmail.com>
2023-03-24 15:36:50 -0700

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,12 +6,11 @@
 */
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 14; j++)
 		{
 			if (j > 9)
 			{
