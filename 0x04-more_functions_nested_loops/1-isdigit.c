e4d02b93b0c57fccccbf825a5e4d12047c3c6840
Happy Banda <hbfl3x@gmail.com>
2023-03-24 13:37:22 -0700

    add 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "mai.h"
+
+/**
+ * _isdigit - prints 1 if character is digit otherwise 0
+ * @c: the charcter to be tested
+ * Return: 1 or 0
+*/
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9 ? 1 : 0);
+}
