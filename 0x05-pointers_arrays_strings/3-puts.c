a2c223458b564c6644f8b85b8ed83842cafd5552
Happy Banda <hbfl3x@gmail.com>
2023-03-27 16:39:05 -0700

    modify 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,18 @@
 #include "main.h"
+#include <string.h>
 
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: the string t be printed
+ * Return: 0 always
+*/
 void _puts(char *str)
 {
-	int str_len = strlen(str);
+	int str_len = strlen(*str);
 
-	for(i = 0; i < str_len; i++)
+	for (i = 0; i < str_len; i++)
 	{
-		_putchar(*str);
+		_putchar(*str[i]);
 	}
 	_puctahr(*str);
 }
