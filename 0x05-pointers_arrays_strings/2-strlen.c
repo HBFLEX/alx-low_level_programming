edb584a540da45f75810e995c36dda27afd3726b
Happy Banda <hbfl3x@gmail.com>
2023-03-27 15:39:20 -0700

    add 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+/**
+ * _strlen - returns the a string length
+ * @s: the string to be processed
+ * Return: string length
+*/
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
