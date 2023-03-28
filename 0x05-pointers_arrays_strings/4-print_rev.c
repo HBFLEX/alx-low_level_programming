c52c6090ebb822ba57cda11d20e04b2f0fbccc41
Happy Banda <hbfl3x@gmail.com>
2023-03-27 17:20:25 -0700

    add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+
+/**
+ * print_rev - prints a reversed string
+ * @s: the string to be printed
+ * Return: 0 always
+*/
+void print_rev(char *s)
+{
+	int str_len = 0;
+	int i;
+
+	while (*s != "\0")
+	{
+		str_len++;
+	}
+
+	for (i = str_len; i >= 0; i--)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
