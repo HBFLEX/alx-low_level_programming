6e137a6745af46f34e45d77fc4537c8e33ce1ced
Happy Banda <hbfl3x@gmail.com>
2023-03-28 14:23:38 -0700

    add 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,24 @@
 #include "main.h"
 
+void printnumber(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = - n;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+
+	if (n / 10)
+	{
+		printnumber(n / 10);
+	}
+	putchar(n % 10 + '0');
+}
 
 void print_array(int *a, int n)
 {
@@ -9,13 +28,13 @@ void print_array(int *a, int n)
 	{
 		if (i == n - 1)
 		{
-			_putchar((*(a + i)) + '0');
+			printnumber((*(a + i)) + '0');
 			_putchar('\n');
 			break;
 		}
 		else
 		{
-			_putchar((*(a + i)) + '0');
+			printnumber((*(a + i)) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
