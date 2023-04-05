13c1f5c79984eeeaa822c3a2fd50567a53f10573
Happy Banda <hbfl3x@gmail.com>
2023-04-05 15:08:39 -0700

    add 0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: the string input
+ * Return: the string output
+*/
+void _puts_recursion(char *s)
+{
+	if (strlen(s) == 0)
+		_putchar('\n');
+
+	_putchar(s[0]) + _puts_recursion(s + 1);
+}
