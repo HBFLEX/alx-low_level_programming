95ba764ff6a90edf9fe772da7789833c4fb7644c
Happy Banda <hbfl3x@gmail.com>
2023-04-05 16:12:17 -0700

    add 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,12 +5,10 @@
  * _strlen_recursion - returns the length of a string.
  * @s: the string input
  * Return: the string length
-
 */
 int _strlen_recursion(char *s)
 {
 	if (strlen(s) == 0)
-		return 0;
-	
+		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
