a889e54aca238c747b0d55aac4a303218df38eef
Happy Banda <hbfl3x@gmail.com>
2023-04-11 07:31:19 -0700

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 or 1
+*/
+int main(int argc, char **argv)
+{
+	int firstNumber, secondNumber, result;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	firstNumber = atoi(argv[1]);
+	secondNumber = atoi(argv[2]);
+	result = firstNumber * secondNumber;
+
+	printf("%d\n", result);
+	return (0);
+}
