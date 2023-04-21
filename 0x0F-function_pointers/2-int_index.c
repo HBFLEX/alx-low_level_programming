df44a210f2d21d964e111e87b654b69934db7a11
Happy Banda <hbfl3x@gmail.com>
2023-04-21 09:22:03 -0700

    fix 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return;
 	for (i = 0; i < size; i++)
+	{
 		if (cmp(array[i]))
 			return (i);
+	}
 	return (-1);
 }
